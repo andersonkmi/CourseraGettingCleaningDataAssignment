1358e9e7495db92c53b0830bb2e9e8af1d0182bd
Anderson Keiji Matsuki Ito <anderson.ito@bcash.com.br>
2016-04-23 22:54:01 -0500

    Modified script

--- run_analysis.R
+++ run_analysis.R
@@ -5,8 +5,14 @@
 library(data.table)
 downloadedFile <- "uci-har-dataset.zip"
 
-if (!file.exists(filename)){
+if (!file.exists(downloadedFile)){
   fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip "
   download.file(fileURL, downloadedFile)
-  unzip(filename)
+  unzip(downloadedFile)
 }
+
+###########################################
+### Global activity and feature loading
+###########################################
+activity_info <- data.table(read.table(file = "UCI HAR Dataset/activity_labels.txt"))
+feature_list <- data.table(read.table(file = "UCI HAR Dataset/features.txt"))
